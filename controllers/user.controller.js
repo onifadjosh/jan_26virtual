83e10f6851e06f702a9430a23f3df3ec7312b13c
onifadjosh <onifadjosh@gmail.com>
2026-02-14 09:54:10 +0100

    Updated user controller and routes to include password hashing, JWT authentication, and user deletion functionality

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,56 +1,129 @@
-const UserModel = require("../models/user.model")
+const UserModel = require("../models/user.model");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
+const createUser = async (req, res) => {
+  const { firstname, lastname, email, password } = req.body;
 
-const createUser=async(req, res)=>{
-    const {firstname, lastname, email, password} = req.body
+  try {
+    let salt = 10;
+    const saltRound = await bcrypt.genSalt(salt);
 
-    try {
-        const user =await UserModel.create(req.body)
-    res.status(201).send(
-        {
-            message:"user created successfully",
-            data:{
-                firstname,
-                lastname,
-                email
-            }
-        }
-    )
-    } catch (error) {
-        console.log(error);
+    const hashedPassword = await bcrypt.hash(password, saltRound);
+    const user = await UserModel.create({
+      firstname,
+      lastname,
+      email,
+      password: hashedPassword,
+    });
+    const token = await jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "5h",
+    });
+    res.status(201).send({
+      message: "user created successfully",
+      data: {
+        firstname,
+        lastname,
+        email,
+      },
+      token,
+    });
+  } catch (error) {
+    console.log(error);
 
-        res.status(400).send({
-            message:"Account creation failed"
-        })
-        
+    res.status(400).send({
+      message: "Account creation failed",
+    });
+  }
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const isUser = await UserModel.findOne({ email });
+
+    if (!isUser) {
+      res.status(404).send({
+        message: "Invalid credentials",
+      });
+
+      return;
+    }
+    console.log(isUser);
+
+    const isMatch = await bcrypt.compare(password, isUser.password);
+    if (!isMatch) {
+      res.status(404).send({
+        message: "Invalid credentials",
+      });
+      return;
     }
 
+    const token = await jwt.sign({ id: isUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "5h",
+    });
+    res.status(200).send({
+      message: "User login successful",
+      data: {
+        firstname: isUser.firstname,
+        lastname: isUser.lastname,
+        email: isUser.email,
+      },
+      token,
+    });
+  } catch (error) {
+    console.log(error);
 
+    res.status(404).send({
+      message: "Invalid credentials",
+    });
+  }
+};
 
-}
+const getUsers = async (req, res) => {
+  try {
+    let user = await UserModel.find().select("-password");
+    res.status(200).send({
+      message: "All users fetched successfully",
+      data: user,
+    });
+  } catch (error) {
+    console.log(error);
 
+    res.status(404).send({
+      message: "cannot get users at this time",
+    });
+  }
+};
 
-const getUsers=async(req, res)=>{
-    try {
-        let user = await UserModel.find().select("-password")
-        res.status(200).send({
-            message:"All users fetched successfully",
-            data:user
-        })
-    } catch (error) {
-        console.log(error);
+const deleteUser = async (req, res) => {
+  const { id } = req.params;
+  try {
+    let user = await UserModel.findByIdAndDelete(id);
 
-        res.status(404).send({
-            message:"cannot get users at this time"
-        })
-        
-    }
-}
+    res.status(204).send({
+      message: "User deleted",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(404).send({
+      message: "User cannot be deleted",
+    });
+  }
+};
 
+//to save user, create the user with hashed password before saving it into the data base
+//Authentication and authorization
 
+const verifyUser = (req, res, next) => {
+  const token = req.headers.authorization.split(" ")[1]
+    ? req.headers.authorization.split(" ")[1]
+    : req.headers.authorization.split(" ")[0];
+};
 
-
-module.exports= {
-    createUser,
-    getUsers
-}
+module.exports = {
+  createUser,
+  getUsers,
+  deleteUser,
+  login,
+};
--- routers/user.route.js
+++ routers/user.route.js
@@ -1,9 +1,11 @@
 const express= require('express')
-const { createUser, getUsers } = require('../controllers/user.controller')
+const { createUser, getUsers, deleteUser, login } = require('../controllers/user.controller')
 
 const router = express.Router()
 
 router.post('/signUp', createUser )
 router.get('/getUsers', getUsers)
+router.delete("/deleteUser/:id", deleteUser)
+router.post("/login", login)
 
 module.exports= router
